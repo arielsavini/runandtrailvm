599f7f6500527666593a18e692143b0d024e569f
Claudio Ariel Savini <arielsavini@gmail.com>
2026-04-01 13:51:29 -0300

    update: index.html y sw.js desde desarrollo-index

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Push Notifications: muestra notificaciones y maneja clicks
  */
 
-const CACHE_NAME    = 'run-trail-vm-v1';
+const CACHE_NAME    = 'run-trail-vm-v4';
 const STATIC_ASSETS = [
   './',
   './index.html',
